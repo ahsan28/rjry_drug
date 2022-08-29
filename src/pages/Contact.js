64ccfc44bb220ccc04ac0d61911c49f4c38bbc31
ahsan28 <ahabib.j@gmail.com>
2022-08-29 13:17:17 +0600

    Add contact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,44 +1,156 @@
-import { Box, Typography } from '@mui/material'
-import React from 'react'
+import {
+  Button,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Grid,
+  MenuItem,
+  Radio,
+  RadioGroup,
+  Select,
+  Slider,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { Box } from "@mui/system";
+import React, { useState } from "react";
 
+const defaultValues = {
+  name: "",
+  age: 0,
+  gender: "",
+  os: "",
+  favoriteNumber: 0,
+};
 const Contact = () => {
+  const [formValues, setFormValues] = useState(defaultValues);
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+  const handleSliderChange = (name) => (e, value) => {
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formValues);
+  };
   return (
-    <Box sx={{ width: '100%', maxWidth: 500 }}>
+    <Box sx={{ width: "100%", maxWidth: 500 }}>
       <Typography variant="h1" gutterBottom>
-      Contact
+        Contact
       </Typography>
-
-      <Typography variant="subtitle1" gutterBottom>
-        subtitle1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
-        blanditiis tenetur
-      </Typography>
-      <Typography variant="subtitle2" gutterBottom>
-        subtitle2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
-        blanditiis tenetur
-      </Typography>
-      <Typography variant="body1" gutterBottom>
-        body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
-        blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,
-        neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum
-        quasi quidem quibusdam.
-      </Typography>
-      <Typography variant="body2" gutterBottom>
-        body2. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
-        blanditiis tenetur unde suscipit, quam beatae rerum inventore consectetur,
-        neque doloribus, cupiditate numquam dignissimos laborum fugiat deleniti? Eum
-        quasi quidem quibusdam.
-      </Typography>
-      <Typography variant="button" display="block" gutterBottom>
-        button text
-      </Typography>
-      <Typography variant="caption" display="block" gutterBottom>
-        caption text
-      </Typography>
-      <Typography variant="overline" display="block" gutterBottom>
-        overline text
-      </Typography>
+      <form onSubmit={handleSubmit}>
+        <Grid container alignItems="center" justify="center" direction="column">
+          <Grid item>
+            <TextField
+              id="name-input"
+              name="name"
+              label="Name"
+              type="text"
+              value={formValues.name}
+              onChange={handleInputChange}
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              id="age-input"
+              name="age"
+              label="Age"
+              type="number"
+              value={formValues.age}
+              onChange={handleInputChange}
+            />
+          </Grid>
+          <Grid item>
+            <FormControl>
+              <FormLabel>Gender</FormLabel>
+              <RadioGroup
+                name="gender"
+                value={formValues.gender}
+                onChange={handleInputChange}
+                row
+              >
+                <FormControlLabel
+                  key="male"
+                  value="male"
+                  control={<Radio size="small" />}
+                  label="Male"
+                />
+                <FormControlLabel
+                  key="female"
+                  value="female"
+                  control={<Radio size="small" />}
+                  label="Female"
+                />
+                <FormControlLabel
+                  key="other"
+                  value="other"
+                  control={<Radio size="small" />}
+                  label="Other"
+                />
+              </RadioGroup>
+            </FormControl>
+          </Grid>
+          <Grid item>
+            <FormControl>
+              <Select
+                name="os"
+                value={formValues.os}
+                onChange={handleInputChange}
+              >
+                <MenuItem key="mac" value="mac">
+                  Mac
+                </MenuItem>
+                <MenuItem key="windows" value="windows">
+                  Windows
+                </MenuItem>
+                <MenuItem key="linux " value="linux">
+                  Linux
+                </MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item>
+            <div style={{ width: "400px" }}>
+              Favorite Number
+              <Slider
+                value={formValues.favoriteNumber}
+                onChange={handleSliderChange("favoriteNumber")}
+                defaultValue={1}
+                step={1}
+                min={1}
+                max={3}
+                marks={[
+                  {
+                    value: 1,
+                    label: "1",
+                  },
+                  {
+                    value: 2,
+                    label: "2",
+                  },
+                  {
+                    value: 3,
+                    label: "3",
+                  },
+                ]}
+                valueLabelDisplay="off"
+              />
+            </div>
+          </Grid>
+          <Button variant="contained" color="primary" type="submit">
+            Submit
+          </Button>
+        </Grid>
+      </form>
     </Box>
-  )
-}
-
-export default Contact
+  );
+};
+export default Contact;
